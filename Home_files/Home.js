f136b37ca129210c23252dca3ab44c7d74701774
Chris Foster <chrisfoster10@me.com>
2013-09-23 20:37:24 +0000

    Initial commit

---
+++ Home_files/Home.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20121116
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Home_files/HomeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id2');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
